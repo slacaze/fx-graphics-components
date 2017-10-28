4f3e2d19389ef136eed66f515b39317bc7c2d63c
Sylvain Lacaze <sylvain.lacaze@mathworks.co.uk>
2017-10-29 00:19:13 +0100

    Simlpe tests for demo code coverage.

--- test/+fx/+graphics/+unittest/tWithTableData.m
+++ test/+fx/+graphics/+unittest/tWithTableData.m
@@ -33,6 +33,39 @@ classdef tWithTableData < matlab.unittest.TestCase
             this.verifyEqual( myTable.Properties.RowNames, {'1', '2', '3'}' );
         end
         
+        function this = testGetTableWithRowNames( this )
+            base = uitable(...
+                'Parent', [] );
+            enhance( base, 'fx.graphics.WithTableData' );
+            base.Data = {1 false 'a';2 false 'b';3 true 'c'};
+            base.ColumnName = {'Numbers', 'Logical', 'Strings'};
+            base.RowName = {'Monday', 'Tuesday', 'Wednesday'};
+            myTable = base.TableData;
+            this.verifyEqual( table2cell( myTable ), {1, false, 'a'; 2, false, 'b'; 3, true, 'c'} );
+            this.verifyEqual( myTable.Properties.VariableNames, {'Numbers', 'Logical', 'Strings'} );
+            this.verifyEqual( myTable.Properties.RowNames, {'Monday', 'Tuesday', 'Wednesday'}' );
+        end
+        
+        function this = testBaseDiesWithComponent( this )
+            base = uitable(...
+                'Parent', [] );
+            enhance( base, 'fx.graphics.WithTableData' );
+            component = base.FxComponents;
+            delete( component );
+            this.verifyFalse( isvalid( base ) );
+        end
+        
+        function this = testErrorOnFxComponentsOverride( this )
+            this.applyFixture(...
+                matlab.unittest.fixtures.SuppressedWarningsFixture( 'MATLAB:callback:DynamicPropertyEventError' ) );
+            base = uitable(...
+                'Parent', [] );
+            enhance( base, 'fx.graphics.WithTableData' );
+            expectedComponents = base.FxComponents;
+            base.FxComponents = 2;
+            this.verifyEqual( base.FxComponents, expectedComponents );
+        end
+        
     end
     
 end
